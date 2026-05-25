3d6ad3f0b92ec03f82c0952d8ef2455e89dc6b79
YourCorrectUsername <galaxydragon259@gmail.com>
2026-05-25 18:58:05 +0800

    started login&sginup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,49 +2,83 @@ import { useNavigate } from "react-router-dom"
 
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import { useState } from "react";
 
 
 
 function Login() {
+
+    const [Button, setButton] = useState(true)
     const navigate = useNavigate();
 
     return (
         <div className="min-h-screen bg-[linear-gradient(to_top_right,#03448C,#19B48E)] text-white">
 
             <Header />
+            {Button ? <div className="  section-margin-x flex flex-row gap-10 content-margint-top ">
 
-            <div className="flex flex-col items-center justify-center text-center px-6 pt-24">
+                <div className="flex-1 flex flex-col px-10 py-10  bg-[#ffffff21] border border-none rounded-2xl">
+                    <div className="flex-1 flex justify-start items-center">
+                        <img
+                            className="p-10 bg-[#ffffff21] rounded-2xl"
+                            src="/logo.png"
+                        />
+                    </div>
+                    <div className="flex flex-col gap-3">
+                        <h2
+                            className="font-bold"
+                        >Welcome Back</h2>
+                        <h1
+                            className="text-7xl font-bold text-[#FFF]"
+                        >CAP PEBBLES</h1>
+                        <p
+                            className="text-[#ffffffc2]"
+                        >Socials: cappebbles@sti.edu | CapPebbles Official</p>
+                        <p
+                            className="text-[#ffffffc2]"
+                        >copyright | 2026 CapPebbles. All rights reserved.</p>
+                    </div>
 
-                <h1 className="text-9xl md:text-8xl font-black leading-none mt-4">
-                    LOGIN
-                </h1>
+                </div>
 
-                <p className="max-w-xl mt-6 text-sm md:text-base text-white/90 text-lg">
-                    Please enter your credentials to access your account.
-                </p>
+                <div className="w-full max-w-md bg-[rgba(222,255,255,0.9)] flex flex-col py-10 px-10 rounded-2xl">                    <h1 className="text-center text-[#0f3c3a] font-bold text-5xl">Log in</h1>
+                    <form className="mt-10 flex flex-col gap-3">
+                        <label className="auth-text">School Email</label>
+                        <input
+                            className="login-inputs"
+                            type="text" name="name" required
+                            placeholder="example@school.edu"
+                        />
 
-                <input
-                    type="text"
-                    placeholder="Email"
-                    className="flex-1 px-5 py-3 my-3 rounded-2xl bg-[#C0F2F1] text-[#03448C] placeholder-[#03448C]/60 outline-none shadow-md"
-                />
+                        <label className="auth-text">Password</label>
+                        <input
+                            className="login-inputs"
+                            type="password"
+                            placeholder="••••••••" />
 
-                <input
-                    type="password"
-                    placeholder="Password"
-                    className="flex-1 px-5 py-3 my-3 rounded-2xl bg-[#C0F2F1] text-[#03448C] placeholder-[#03448C]/60 outline-none shadow-md"
-                />
+                        <button
+                            type="submit"
+                            className="auth-button bg-[#21ad7f]"
+                        > Log in</button>
+                        <p className=" text-center text-black">Or</p>
 
-                <button className="bg-[#19B48E] px-6 py-3 rounded-xl font-semibold hover:scale-105 transition"
-                onClick={() => navigate("/home")}>
-                    Login
-                </button>
+                    </form>
+                    <button
+                        className=" bg-white auth-button text-black "
+                        onClick={()=> setButton(false)}
+                    > Get Started </button>
 
-            </div>
+                </div>
+            </div> :
+                <div>
+                    MAMAYA NA
+                </div>}
 
             {/* <Footer /> */}
-
         </div>
+
+
+
     )
 }
 
